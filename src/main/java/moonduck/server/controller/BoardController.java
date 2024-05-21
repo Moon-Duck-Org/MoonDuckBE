231bf1a73ff1d4d66ac0da33cf979c44d6d618b8
Comesfullcircle <olerlmin1234@gmail.com>
2024-05-21 13:42:04 +0900

    docs: swagger API 명세서 도입

--- src/main/java/moonduck/server/controller/BoardController.java
+++ src/main/java/moonduck/server/controller/BoardController.java
@@ -1,5 +1,6 @@
 package moonduck.server.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import moonduck.server.entity.Board;
 import moonduck.server.repository.BoardRepository;
@@ -16,18 +17,21 @@ public class BoardController {
     private final BoardRepository boardRepository;
 
     //Create 생성
+    @Operation(summary = "리뷰 생성", description = "리뷰를 생성합니다.", tags = {"Board"})
     @PostMapping("/create")
     public ResponseEntity<?> createBoard(Board board){
         return ResponseEntity.ok(boardRepository.save(board));
     }
 
     //ReadAll 리스트 페이지
+    @Operation(summary = "리뷰 리스트", description = "리뷰 리스트를 가져옵니다.", tags = {"Board"})
     @GetMapping("/list")
     public List<Board> readAll(){
         return boardRepository.findAll();
     }
 
     //Read
+    @Operation(summary = "리뷰 디테일", description = "리뷰 하나의 상세 정보를 가져옵니다.", tags = {"Board"})
     @GetMapping("/list/{id}")
     public Board readBoard(@PathVariable Long id) throws IllegalAccessException {
         return boardRepository.findById(id)
@@ -45,6 +49,7 @@ public class BoardController {
     }*/
 
     //Delete
+    @Operation(summary = "리뷰 삭제", description = "리뷰를 삭제합니다.", tags = {"Board"})
     @DeleteMapping("/{id}")
     public void deleteBoard(@PathVariable Long id){
         boardRepository.deleteById(id);
