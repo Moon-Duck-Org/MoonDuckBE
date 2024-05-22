79f783e00d718f2c271353837e4a8cac06d7296d
Comesfullcircle <olerlmin1234@gmail.com>
2024-05-22 16:04:28 +0900

    docs: swagger API 명세서 작성

--- src/main/java/moonduck/server/controller/HomeController.java
+++ src/main/java/moonduck/server/controller/HomeController.java
@@ -1,9 +1,11 @@
 package moonduck.server.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "홈 API", description = "서버 작동 확인을 위한 테스트 API입니다.")
 @RestController
 public class HomeController {
 
