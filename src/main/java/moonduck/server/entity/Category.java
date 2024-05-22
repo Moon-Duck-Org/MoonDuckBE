98f75f61890facccc85b7297804586d424f7a7e4
ajroot5685 <anjeong8577@naver.com>
2024-05-22 11:22:24 +0900

    feat: category 엔티티 enum으로 수정

--- src/main/java/moonduck/server/entity/Category.java
+++ src/main/java/moonduck/server/entity/Category.java
@@ -1,24 +1,9 @@
 package moonduck.server.entity;
 
-import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
-import org.hibernate.annotations.Comment;
-import org.hibernate.sql.Insert;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.data.jpa.repository.Query;
-
-@Getter
-@Setter
-@Entity
-@Table(name = "category", schema="myschema")
-public class Category {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long category_id;
-
-    @Comment("카테고리")
-    @Column(nullable = false)
-    private String category;
+public enum Category {
+    MOVIE,
+    BOOK,
+    DRAMA,
+    CONCERT,
+    ANIMATION
 }
