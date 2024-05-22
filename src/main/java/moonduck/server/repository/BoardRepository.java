483e76ece3010c04c903e0bc408a03d70743d13c
Comesfullcircle <olerlmin1234@gmail.com>
2024-05-22 15:37:08 +0900

    feat: 유저 조회 API

--- src/main/java/moonduck/server/repository/BoardRepository.java
+++ src/main/java/moonduck/server/repository/BoardRepository.java
@@ -1,11 +1,7 @@
 package moonduck.server.repository;
 
-import jakarta.transaction.Transactional;
 import moonduck.server.entity.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
 }
