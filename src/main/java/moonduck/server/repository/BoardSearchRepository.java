5fb80d7674ea5d43d2a02b79a85210e83121e7b5
ajroot5685 <anjeong8577@naver.com>
2024-05-25 21:40:20 +0900

    feat: 리뷰 전체 조회 필터 추가

---
+++ src/main/java/moonduck/server/repository/BoardSearchRepository.java
@@ -0,0 +1,48 @@
+package moonduck.server.repository;
+
+import com.querydsl.core.types.Order;
+import com.querydsl.core.types.OrderSpecifier;
+import com.querydsl.core.types.dsl.PathBuilder;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import moonduck.server.entity.Board;
+import moonduck.server.entity.QBoard;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class BoardSearchRepository {
+
+    private final JPAQueryFactory queryFactory;
+
+    public BoardSearchRepository(EntityManager em) {
+        this.queryFactory = new JPAQueryFactory(em);
+    }
+
+    public List<Board> findByUserIdWithFilter(Long userId, String filter) {
+        QBoard board = QBoard.board;
+
+        return queryFactory
+                .selectFrom(board)
+                .where(board.user.id.eq(userId))
+                .leftJoin(board.user).fetchJoin()
+                .orderBy(getOrderSpecifier(filter))
+                .fetch();
+    }
+
+    private OrderSpecifier getOrderSpecifier(String filter) {
+        PathBuilder<Board> entityPath = new PathBuilder<>(Board.class, "board");
+
+        if (filter.equals("LATEST")) {
+            return new OrderSpecifier(Order.DESC, entityPath.get("createdAt"));
+        } else if (filter.equals("OLDEST")) {
+            return new OrderSpecifier(Order.ASC, entityPath.get("createdAt"));
+        } else if (filter.equals("RATE")) {
+            return new OrderSpecifier(Order.DESC, entityPath.get("score"));
+        } else {
+            return new OrderSpecifier(Order.DESC, entityPath.get("createdAt"));
+        }
+    }
+}
