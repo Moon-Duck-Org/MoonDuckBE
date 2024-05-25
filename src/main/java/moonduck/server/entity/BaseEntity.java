1d77a25981a44092cc55478a9a2cbef10a670dd7
윤설이 <olerlmin1234@gmail.com>
2024-05-26 03:27:45 +0900

    db 시간 insert 오류 수정 😉

--- src/main/java/moonduck/server/entity/BaseEntity.java
+++ src/main/java/moonduck/server/entity/BaseEntity.java
@@ -5,6 +5,8 @@ import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import org.hibernate.annotations.Comment;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -18,11 +20,13 @@ public class BaseEntity {
 
     @Comment("생성날짜")
     @CreatedDate
+    @CreationTimestamp
     @Column(name = "created_at", updatable = false)
     private Timestamp createdAt;
 
     @Comment("수정날짜")
     @LastModifiedDate
+    @UpdateTimestamp
     @Column(name = "modified_at")
     private Timestamp modifiedAt;
 
