258c83861a479b300fab58e78b622492152057a6
윤설이 <olerlmin1234@gmail.com>
2024-06-01 06:47:15 +0900

    openapi 책 카테고리 관련 vo 생성📚

---
+++ src/main/java/moonduck/server/vo/NaverResultVO.java
@@ -0,0 +1,18 @@
+package moonduck.server.vo;
+
+import lombok.*;
+
+import java.util.List;
+
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+public class NaverResultVO {
+    private String lastBuildDate;
+    private int total;
+    private int start;
+    private int display;
+    private List<BookVO> items;
+}
