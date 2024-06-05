6b673a0227e6ce6f29676e72957b7ca21ce3bd99
Comesfullcircle <olerlmin1234@gmail.com>
2024-06-05 13:10:38 +0900

    feat: 유저 인증 컨트롤러 로직 뼈대 추가

--- src/main/java/moonduck/server/config/SecurityConfig.java
+++ src/main/java/moonduck/server/config/SecurityConfig.java
@@ -13,7 +13,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
@@ -34,11 +33,6 @@ public class SecurityConfig {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
         return configuration.getAuthenticationManager();
-    }
-
-    @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
     }
 
     @Bean
@@ -65,7 +59,8 @@ public class SecurityConfig {
 
                 .authorizeHttpRequests((auth) -> auth
                         .requestMatchers("/").permitAll()
-                        .requestMatchers("/user/login").permitAll()
+                        .requestMatchers("/auth/login").permitAll()
+                        .requestMatchers("/auth/reissue").permitAll()
                 )
                 .authorizeHttpRequests((auth) -> auth
                         .requestMatchers("/swagger-ui.html").permitAll()
