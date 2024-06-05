f38e27967447b6dcc43477191c31996ec6de9bd8
Comesfullcircle <olerlmin1234@gmail.com>
2024-06-05 14:12:56 +0900

    feat: jwt 다중 토큰 추가

--- src/main/java/moonduck/server/controller/impl/AuthController.java
+++ src/main/java/moonduck/server/controller/impl/AuthController.java
@@ -5,6 +5,9 @@ import lombok.extern.slf4j.Slf4j;
 import moonduck.server.dto.UserLoginDTO;
 import moonduck.server.dto.auth.ReissueDTO;
 import moonduck.server.dto.auth.TokenResponseDTO;
+import moonduck.server.entity.User;
+import moonduck.server.jwt.JWTUtil;
+import moonduck.server.service.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,8 +21,16 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class AuthController {
 
+    private final UserService userService;
+    private final JWTUtil jwtUtil;
+
     @PostMapping("/login")
     public ResponseEntity<Boolean> login(@RequestBody UserLoginDTO userInfo) {
+        User user = userService.tryRegistrationAndReturnUser(userInfo);
+
+        if (user.getNickname() == null) {
+            return ResponseEntity.ok(false);
+        }
 
         return ResponseEntity.ok(true);
     }
