767e1c7cc38620b22a396d425f1c8733c4619fae
ajroot5685 <anjeong8577@naver.com>
2024-06-06 15:08:36 +0900

    refact: 컨트롤러 인터페이스 분리

---
+++ src/main/java/moonduck/server/controller/impl/BoardApiController.java
@@ -0,0 +1,91 @@
+package moonduck.server.controller.impl;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import moonduck.server.controller.BoardAPI;
+import moonduck.server.dto.request.BoardEditRequest;
+import moonduck.server.dto.request.BoardRequest;
+import moonduck.server.entity.Board;
+import moonduck.server.service.s3.S3Service;
+import moonduck.server.service.BoardService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+public class BoardApiController implements BoardAPI {
+
+    private final BoardService boardService;
+    private final S3Service s3Service;
+
+    //Create 생성
+    @Override
+    public ResponseEntity<Board> savePost(MultipartFile[] images, BoardRequest boardDto) {
+        List<String> imageFiles = s3Service.uploadFiles(images, boardDto.getUserId());
+        Board board = boardService.savePost(imageFiles, boardDto);
+
+        return ResponseEntity.ok(board);
+    }
+
+
+    //Update 수정
+    @Override
+    public ResponseEntity<Board> updatePost(MultipartFile[] images, BoardEditRequest boardDto) {
+        List<String> imageFiles = (images == null ? new ArrayList<>() : s3Service.uploadFiles(images, boardDto.getUserId()));
+        Board editedBoard = boardService.update(imageFiles, boardDto);
+
+        return ResponseEntity.ok(editedBoard);
+    }
+
+    //Read
+    @Override
+    public ResponseEntity<Page<Board>> findPosts(Long userId, String filter, int offset, int size){
+        Pageable pageable = PageRequest.of(offset, size);
+        Page<Board> reviews = boardService.getAllReview(userId, filter, pageable);
+
+        return ResponseEntity.ok(reviews);
+    }
+
+    // 카테고리별 리스트 조회
+    @Override
+    public ResponseEntity<Page<Board>> search(Long userId, String category, String filter, int offset, int size) {
+        Pageable pageable = PageRequest.of(offset, size);
+        Page<Board> reviewWithCategory = boardService.getReviewWithCategory(userId, category, filter, pageable);
+
+        return ResponseEntity.ok(reviewWithCategory);
+    }
+
+
+
+    //Read
+    @Override
+    public ResponseEntity<Board> findPost(Long boardId){
+        Board review = boardService.getReview(boardId);
+
+        return ResponseEntity.ok(review);
+    }
+
+
+    //Delete
+    @Override
+    public ResponseEntity<Boolean> delete(Long boardId){
+        boardService.deletePost(boardId);
+
+        return ResponseEntity.ok(true);
+    }
+}
--- src/main/java/moonduck/server/controller/impl/UserController.java
+++ src/main/java/moonduck/server/controller/impl/UserController.java
@@ -1,33 +1,24 @@
-package moonduck.server.controller;
+package moonduck.server.controller.impl;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.ExampleObject;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import moonduck.server.controller.UserAPI;
 import moonduck.server.dto.request.UserEditRequest;
 import moonduck.server.dto.response.UserInfoResponse;
-import moonduck.server.dto.request.LoginRequest;
 import moonduck.server.entity.User;
 import moonduck.server.service.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
-@Tag(name = "유저 API", description = "유저 관련 API")
-@ApiResponse(responseCode = "200", description = "OK")
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/user")
 @Slf4j
-public class UserController {
+public class UserController implements UserAPI {
 
     private final UserService userService;
 
-    @Operation(summary = "닉네임 수정", description = "디바이스 id에 해당하는 유저의 닉네임을 수정합니다.")
-    @PutMapping("/nickname")
+    @Override
     public ResponseEntity<User> editNickname(@RequestBody UserEditRequest userEditInfo) {
         // 해당 이메일이 존재하지 않다면 4xx 반환
         // 존재한다면 해당 유저의 닉네임 수정
@@ -36,8 +27,7 @@ public class UserController {
         return ResponseEntity.ok(editedUser);
     }
 
-    @Operation(summary = "회원 조회", description = "디바이스 id에 해당하는 유저 정보를 조회합니다.")
-    @GetMapping("")
+    @Override
     public ResponseEntity<UserInfoResponse> getUserInfo(
 //            @RequestParam(name = "deviceId")
 //            String deviceId
