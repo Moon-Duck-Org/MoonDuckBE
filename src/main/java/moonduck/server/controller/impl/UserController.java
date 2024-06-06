16f989ef29aae7d3b098568ebce1d87eaf5dea35
ajroot5685 <anjeong8577@naver.com>
2024-06-06 21:26:44 +0900

    refact: 유저 응답 추상화

--- src/main/java/moonduck/server/controller/impl/UserController.java
+++ src/main/java/moonduck/server/controller/impl/UserController.java
@@ -5,7 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import moonduck.server.controller.UserAPI;
 import moonduck.server.dto.request.UserEditRequest;
 import moonduck.server.dto.response.UserInfoResponse;
-import moonduck.server.entity.User;
+import moonduck.server.dto.response.UserResponse;
 import moonduck.server.service.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,17 +18,16 @@ public class UserController implements UserAPI {
     private final UserService userService;
 
     @Override
-    public ResponseEntity<User> editNickname(Long userId, UserEditRequest userEditInfo) {
-        User editedUser = userService.editNickname(userId, userEditInfo.getNickname());
-
-        return ResponseEntity.ok(editedUser);
-    }
-
-    @Override
     public ResponseEntity<UserInfoResponse> getUserInfo(Long userId) {
         UserInfoResponse userInfo = userService.getUser(userId);
 
         return ResponseEntity.ok(userInfo);
     }
 
+    @Override
+    public ResponseEntity<UserResponse> editNickname(Long userId, UserEditRequest userEditInfo) {
+        UserResponse editedUser = userService.editNickname(userId, userEditInfo.getNickname());
+
+        return ResponseEntity.ok(editedUser);
+    }
 }
