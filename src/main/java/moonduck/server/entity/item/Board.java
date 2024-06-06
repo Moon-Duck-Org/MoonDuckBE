344527ea5eb1819d740fe456773f9551147d766b
윤설이 <olerlmin1234@gmail.com>
2024-06-06 21:56:54 +0900

    entity 수정 ! 📝

--- src/main/java/moonduck/server/entity/item/Board.java
+++ src/main/java/moonduck/server/entity/item/Board.java
@@ -1,43 +1,41 @@
-package moonduck.server.entity.item;
+package moonduck.server.entity;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import moonduck.server.entity.BaseEntity;
-import moonduck.server.entity.Category;
-import moonduck.server.entity.User;
+import lombok.*;
+import moonduck.server.dto.BoardEditDTO;
+import moonduck.server.dto.BoardRequestDTO;
 import org.hibernate.annotations.Comment;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Getter
 @Setter
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "board", schema = "myschema")
 @Inheritance(strategy = InheritanceType.JOINED)
-@DiscriminatorColumn(name = "category")
+@Table(name = "board", schema = "myschema")
 @Schema(description = "리뷰 엔티티")
-public abstract class Board extends BaseEntity {
+public class Board extends BaseEntity{
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "board_id")
     private Long id;
-
-    @Comment("제목")
-    @Column(length = 30, nullable = false)
-    private String title;
 
     @Comment("유저 정보")
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     @Schema(description = "유저 정보")
     private User user;
+
+    @Comment("제목")
+    @Column(length = 30, nullable = false)
+    private String title;
+
+    @Comment("카테고리")
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Category category;
 
     @Comment("내용")
     @Column(columnDefinition = "TEXT")
@@ -71,9 +69,19 @@ public abstract class Board extends BaseEntity {
     @Column(nullable = false)
     private Integer score;
 
-    @ManyToMany(mappedBy = "items")
-    private List<Category> categories = new ArrayList<Category>();
-}
+    public Board(BoardRequestDTO boardDto) {
+        this.title = boardDto.getTitle();
+        this.category = boardDto.getCategory();
+        this.content = boardDto.getContent();
+        this.url = boardDto.getUrl();
+        this.score = boardDto.getScore();
+    }
 
-
+    public void updateBoard(BoardEditDTO boardDto) {
+        this.title = boardDto.getTitle();
+        this.category = boardDto.getCategory();
+        this.content = boardDto.getContent();
+        this.url = boardDto.getUrl();
+        this.score = boardDto.getScore();
+    }
 }
