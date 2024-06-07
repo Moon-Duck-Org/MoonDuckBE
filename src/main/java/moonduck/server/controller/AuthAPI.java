420d12c4b016640f2084b76c6bf2d77271a9186c
ajroot5685 <anjeong8577@naver.com>
2024-06-07 11:22:31 +0900

    docs: auth 명세서 수정

--- src/main/java/moonduck/server/controller/AuthAPI.java
+++ src/main/java/moonduck/server/controller/AuthAPI.java
@@ -21,16 +21,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public interface AuthAPI {
 
     @Operation(summary = "로그인", description = "소셜 로그인으로부터 받은 회원 정보로 로그인을 수행합니다. 회원 가입을 자동으로 수행하고 닉네임 설정 여부를 반환합니다.")
-    @ApiResponse(responseCode = "200", description = "OK", content = @Content(
-            mediaType = "application/json",
-            examples = {
-                    @ExampleObject(name = "true", description = "닉네임 설정이 되어 있고, 정상적으로 로그인이 된 경우입니다.",
-                            value = "true"
-                    ),
-                    @ExampleObject(name = "false", description = "로그인은 되었지만, 닉네임 설정이 되어 있지 않아 닉네임 설정이 필요합니다.",
-                            value = "false"
-                    )
-            }))
     @PostMapping("/login")
     ResponseEntity<LoginResponse> login(@RequestBody LoginRequest userInfo);
 
