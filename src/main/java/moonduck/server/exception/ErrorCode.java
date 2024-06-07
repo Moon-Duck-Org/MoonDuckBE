2188a35769af892bfa96b237ca705e20b7d8adbf
Comesfullcircle <olerlmin1234@gmail.com>
2024-06-07 14:30:13 +0900

    feat: 오류 코드 추가

--- src/main/java/moonduck/server/exception/ErrorCode.java
+++ src/main/java/moonduck/server/exception/ErrorCode.java
@@ -7,25 +7,31 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum ErrorCode {
 
-    BOARD_NOT_FOUND(400, "존재하지 않는 리뷰입니다."),
-    CATEGORY_NOT_MATCH(400, "잘못된 카테고리입니다."),
+    /**
+     * 보드 에러
+     */
+    BOARD_NOT_FOUND(400, "BO001", "존재하지 않는 리뷰입니다."),
+    CATEGORY_NOT_MATCH(400, "BO002", "잘못된 카테고리입니다."),
+    WRONG_FILTER(400, "BO003", "잘못된 필터 조건입니다."),
+    FILE_ERROR(500, "BO004", "파일 처리 중 오류가 발생했습니다."),
 
+    /**
+     * 유저 에러
+     */
+    USER_NOT_FOUND(401, "US001", "존재하지 않는 유저입니다."),
+    NICKNAME_DUPLICATE(400, "US002", "중복된 닉네임입니다."),
 
-    USER_NOT_FOUND(401, "존재하지 않는 유저입니다."),
-    NICKNAME_DUPLICATE(400, "중복된 닉네임입니다."),
-
-
-    WRONG_FILTER(400, "잘못된 필터 조건입니다."),
-    FILE_ERROR(500, "파일 처리 중 오류가 발생했습니다."),
-
-
-    NO_TOKEN(400, "토큰이 존재하지 않습니다."),
-    NOT_MATCH_CATEGORY(400, "잘못된 유형의 토큰입니다."),
-    TOKEN_EXPIRED(403, "만료된 토큰입니다."),
-    TOKEN_NOT_EXPIRED(403, "아직 토큰이 만료되지 않았습니다."),
-    INVALID_TOKEN(401, "유효하지 않은 토큰입니다."),
-    UNAUTHENTICATED_USER(401, "인증정보가 등록되지 않았습니다. 서버에 문의해주세요.");
+    /**
+     * 인증 에러
+     */
+    NO_TOKEN(400, "AU001", "토큰이 존재하지 않습니다."),
+    NOT_MATCH_CATEGORY(400, "AU002", "잘못된 유형의 토큰입니다."),
+    TOKEN_EXPIRED(403, "AU003", "만료된 토큰입니다."),
+    TOKEN_NOT_EXPIRED(403, "AU004", "아직 토큰이 만료되지 않았습니다."),
+    INVALID_TOKEN(401, "AU005", "유효하지 않은 토큰입니다."),
+    UNAUTHENTICATED_USER(401, "AU006", "인증정보가 등록되지 않았습니다. 서버에 문의해주세요.");
 
     private final int status;
+    private final String code;
     private final String message;
 }
