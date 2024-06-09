8ff507979d0d9fa0a4ed155ef2573ebc02e6c570
윤설이 <olerlmin1234@gmail.com>
2024-06-09 15:26:49 +0900

    엔티티 수정 중

---
+++ src/main/java/moonduck/server/entity/item/Concert.java
@@ -0,0 +1,59 @@
+package moonduck.server.entity.item;
+
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("C")
+public class Concert extends Item {
+    private String title;
+    private String place;
+    private String date;
+    private String actors;
+    private int price;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPlace() {
+        return place;
+    }
+
+    public void setPlace(String place) {
+        this.place = place;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getActors() {
+        return actors;
+    }
+
+    public void setActors(String actors) {
+        this.actors = actors;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Concert{}";
+    }
+}
