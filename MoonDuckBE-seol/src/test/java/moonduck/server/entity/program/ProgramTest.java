34fa99405bc438096be77f998e8a7af57efa941c
윤설이 <olerlmin1234@gmail.com>
2024-06-09 20:09:36 +0900

    Program 싱글 테이블 entity 생성

---
+++ MoonDuckBE-seol/src/test/java/moonduck/server/entity/program/ProgramTest.java
@@ -0,0 +1,28 @@
+package moonduck.server.entity.program;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProgramTest {
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void setId() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void setName() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
