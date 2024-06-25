d41e6ff29bf1ad027fabd255d866cff2f8053415
윤설이 <olerlmin1234@gmail.com>
2024-06-25 15:32:23 +0900

    수정

--- MoonDuckBE-seol/src/main/java/moonduck/server/repository/ProgramRepository.java
+++
@@ -1,7 +0,0 @@
-package moonduck.server.repository;
-
-import moonduck.server.entity.program.Program;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProgramRepository extends JpaRepository<Program, Long> {
-}
