24db8b303ca8ae3fad64368cc7fb671698c2633c
Comesfullcircle <olerlmin1234@gmail.com>
2024-06-29 15:49:48 +0900

    feat: 회원탈퇴

--- src/main/java/moonduck/server/repository/BoardRepository.java
+++ src/main/java/moonduck/server/repository/BoardRepository.java
@@ -5,12 +5,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
 
     @Query("select b from Board b left join fetch b.user left join fetch b.program where b.id = :id")
     Optional<Board> findByIdWithUserAndProgram(@Param("id") Long id);
+
+    @Query("select b from Board b left join fetch b.user where b.user.id = :uid")
+    List<Board> findAllByUserId(@Param("uid") Long userId);
 
 //    @Query("select b from Board b left join fetch b.user where b.user.id = :userId")
 //    List<Board> findByUserId(@Param("userId") Long userId);
