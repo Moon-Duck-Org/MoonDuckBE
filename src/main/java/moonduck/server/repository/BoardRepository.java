2d0ca9d83eb062d5059d5f8b863542967be367d4
ajroot5685 <anjeong8577@naver.com>
2024-06-29 16:10:30 +0900

    docs: 회원탈퇴 명세서 작성

--- src/main/java/moonduck/server/repository/BoardRepository.java
+++ src/main/java/moonduck/server/repository/BoardRepository.java
@@ -2,6 +2,7 @@ package moonduck.server.repository;
 
 import moonduck.server.entity.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
