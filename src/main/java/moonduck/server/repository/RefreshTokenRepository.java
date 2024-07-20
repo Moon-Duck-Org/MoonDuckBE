d6efe79b0b739333b479fd49fc1b3324e2edb6f2
Comesfullcircle <olerlmin1234@gmail.com>
2024-07-20 15:42:27 +0900

    feat: RTR 도메인/dto 설정

---
+++ src/main/java/moonduck/server/repository/RefreshTokenRepository.java
@@ -0,0 +1,9 @@
+package moonduck.server.repository;
+
+import moonduck.server.entity.RefreshToken;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RefreshTokenRepository extends CrudRepository<RefreshToken, Long> {
+}
