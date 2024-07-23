90e120af1e750652351a9ceb71af09bcf82fbbc2
Comesfullcircle <olerlmin1234@gmail.com>
2024-07-24 00:10:49 +0900

    feat: 레디스 엔티티 정의

---
+++ src/main/java/moonduck/server/entity/Share.java
@@ -0,0 +1,23 @@
+package moonduck.server.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.TimeToLive;
+
+@RedisHash("share")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Share {
+
+    @Id
+    private String uuid;
+
+    private Long boardId;
+
+    @TimeToLive
+    private Long expiration;
+}
